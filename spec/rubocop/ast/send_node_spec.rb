2b6e98ba4be2329289fa333a14c7899147a0d317
Marc-Andre Lafortune <github@marc-andre.ca>
2020-05-26 19:36:55 -0400

    [Fix #12] SendNode#attribute_accessor? requires at least one argument.
    Couldn't think of a better way to not change the capture structure

--- spec/rubocop/ast/send_node_spec.rb
+++ spec/rubocop/ast/send_node_spec.rb
@@ -796,6 +796,14 @@ RSpec.describe RuboCop::AST::SendNode do
           )
         )
       end
+
+      context 'with a call without arguments' do
+        let(:source) { 'attr_reader' }
+
+        it do
+          expect(send_node.attribute_accessor?).to be(nil)
+        end
+      end
     end
   end
 
