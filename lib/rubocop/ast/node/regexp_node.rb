7fc0d09750d0e6ca7bd0f806d226d2431dc87825
Owen Stephens <owen@owenstephens.co.uk>
2020-06-16 09:08:09 +0100

    fixup! fixup! Add `delimiter?' predicate for `RegexpNode`

--- lib/rubocop/ast/node/regexp_node.rb
+++ lib/rubocop/ast/node/regexp_node.rb
@@ -32,9 +32,24 @@ module RuboCop
         children.select(&:str_type?).map(&:str_content).join
       end
 
+      # @return [Bool] if the regexp is a /.../ literal
+      def slash_literal?
+        loc.begin.source == '/'
+      end
+
+      # @return [Bool] if the regexp is a %r{...} literal (using any delimiters)
+      def percent_r_literal?
+        !slash_literal?
+      end
+
+      # @return [String] the regexp delimiters (without %r)
+      def delimiters
+        [loc.begin.source[-1], loc.end.source[0]]
+      end
+
       # @return [Bool] if char is one of the delimiters
       def delimiter?(char)
-        [loc.begin.source[-1], loc.end.source[0]].include?(char)
+        delimiters.include?(char)
       end
 
       # @return [Bool] if regexp contains interpolation
