6b1b46003446a99f2881489857d20d5cb59bce5d
Marc-Andre Lafortune <github@marc-andre.ca>
2020-09-24 16:13:51 -0400

    [Fixes #113] Add basic compatibility check with main gem

---
+++ lib/rubocop/ast/rubocop_compatibility.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+module RuboCop
+  # ...
+  module AST
+    # Responsible for compatibility with main gem
+    # @api private
+    module RuboCopCompatibility
+      INCOMPATIBLE_COPS = {
+        '0.89.0' => 'Layout/LineLength'
+      }.freeze
+      def rubocop_loaded
+        loaded = Gem::Version.new(RuboCop::Version::STRING)
+        incompatible = INCOMPATIBLE_COPS.select do |k, _v|
+          loaded < Gem::Version.new(k)
+        end.values
+        return if incompatible.empty?
+
+        warn <<~WARNING
+          *** WARNING – Incompatible versions of `rubocop` and `rubocop-ast`
+          You may encounter issues with the following \
+          Cop#{'s' if incompatible.size > 1}: #{incompatible.join(', ')}
+          Please upgrade rubocop to at least v#{INCOMPATIBLE_COPS.keys.last}
+        WARNING
+      end
+    end
+
+    extend RuboCopCompatibility
+  end
+end
