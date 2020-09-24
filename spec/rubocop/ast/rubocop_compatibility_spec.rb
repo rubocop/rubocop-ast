aef761bc93e52561cbbf3233b917b8b25b356b6c
Marc-Andre Lafortune <github@marc-andre.ca>
2020-09-24 16:13:51 -0400

    [Fixes #113] Add basic compatibility check with main gem

---
+++ spec/rubocop/ast/rubocop_compatibility_spec.rb
@@ -0,0 +1,25 @@
+# frozen_string_literal: true
+
+RSpec.describe RuboCop::AST::RuboCopCompatibility do # rubocop:disable RSpec/FilePath
+  subject(:callback) { RuboCop::AST.rubocop_loaded }
+
+  before do
+    stub_const '::RuboCop::Version::STRING', rubocop_version
+  end
+
+  context 'when ran from an incompatible version of Rubocop' do
+    let(:rubocop_version) { '0.42.0' }
+
+    it 'issues a warning' do
+      expect { callback }.to output(/LineLength/).to_stderr
+    end
+  end
+
+  context 'when ran from a compatible version of Rubocop' do
+    let(:rubocop_version) { '0.89.0' }
+
+    it 'issues a warning' do
+      expect { callback }.not_to output.to_stderr
+    end
+  end
+end
