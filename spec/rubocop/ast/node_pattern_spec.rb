804eb6f0a03b2b5bde537c32981c2c3d82a97bc9
Marc-Andre Lafortune <github@marc-andre.ca>
2020-09-28 22:52:17 -0400

    Care about backtrace only on MRI

--- spec/rubocop/ast/node_pattern_spec.rb
+++ spec/rubocop/ast/node_pattern_spec.rb
@@ -2066,7 +2066,7 @@ RSpec.describe RuboCop::AST::NodePattern do
       raise_error do |err|
         expect(err.is_a?(ArgumentError)).to be(true)
         expect(err.message).to include('wrong number of arguments')
-        expect(err.backtrace_locations.first.lineno).to be(line_no) unless RUBY_PLATFORM == 'java'
+        expect(err.backtrace_locations.first.lineno).to be(line_no) if RUBY_ENGINE == 'ruby'
       end
     end
 
