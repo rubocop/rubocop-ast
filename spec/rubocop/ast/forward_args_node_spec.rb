1acd287337aeba4e5134e39ef0392135fd4abc5d
Marc-Andre Lafortune <github@marc-andre.ca>
2020-10-15 22:44:02 -0400

    Bypass bug
    See https://github.com/rubocop-hq/rubocop-rspec/pull/1018

--- spec/rubocop/ast/forward_args_node_spec.rb
+++ spec/rubocop/ast/forward_args_node_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe RuboCop::AST::ForwardArgsNode do
   let(:args_node) { parse_source(source).ast.arguments }
   let(:source) { 'def foo(...); end' }
 
-  context 'when using Ruby 2.7 or newer', :ruby27 do
+  context 'when using Ruby 2.7 or newer', :ruby27 do # rubocop:disable RSpec/EmptyExampleGroup
     if RuboCop::AST::Builder.emit_forward_arg
       describe '#to_a' do
         it { expect(args_node.to_a).to contain_exactly(be_forward_arg_type) }
