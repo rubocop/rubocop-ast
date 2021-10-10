e90689beea828c2654ba2449bccae87ca6120413
Koichi ITO <koic.ito@gmail.com>
2021-10-10 20:12:27 +0900

    Add spell checking GitHub Actions workflow
    Follow up to https://github.com/rubocop/rubocop/pull/10128.
    
    This PR adds spell checking GitHub Actions workflow.

--- lib/rubocop/ast/node/self_class_node.rb
+++ lib/rubocop/ast/node/self_class_node.rb
@@ -6,9 +6,9 @@ module RuboCop
     # plain node when the builder constructs the AST, making its methods
     # available to all `sclass` nodes within RuboCop.
     class SelfClassNode < Node
-      # The identifer for this `sclass` node. (Always `self`.)
+      # The identifier for this `sclass` node. (Always `self`.)
       #
-      # @return [Node] the identifer of the class
+      # @return [Node] the identifier of the class
       def identifier
         node_parts[0]
       end
