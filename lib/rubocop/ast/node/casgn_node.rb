742450b0fb71d354f99dd7b88ed75a5688534186
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2024-10-28 17:50:11 -0400

    Refactor `CasgnNode` and `ConstNode` to extract common functionality to `ConstantNode` mixin.

--- lib/rubocop/ast/node/casgn_node.rb
+++ lib/rubocop/ast/node/casgn_node.rb
@@ -6,19 +6,9 @@ module RuboCop
     # This will be used in place of a plain node when the builder constructs
     # the AST, making its methods available to all assignment nodes within RuboCop.
     class CasgnNode < Node
-      # The namespace of the constant being assigned.
-      #
-      # @return [Node, nil] the node associated with the scope (e.g. cbase, const, ...)
-      def namespace
-        node_parts[0]
-      end
+      include ConstantNode
 
-      # The name of the variable being assigned as a symbol.
-      #
-      # @return [Symbol] the name of the variable being assigned
-      def name
-        node_parts[1]
-      end
+      alias name short_name
 
       # The expression being assigned to the variable.
       #
--- spec/rubocop/ast/casgn_node_spec.rb
+++ spec/rubocop/ast/casgn_node_spec.rb
@@ -43,6 +43,14 @@ RSpec.describe RuboCop::AST::CasgnNode do
     it { is_expected.to eq(:VAR) }
   end
 
+  describe '#short_name' do
+    subject { casgn_node.short_name }
+
+    let(:source) { 'VAR = value' }
+
+    it { is_expected.to eq(:VAR) }
+  end
+
   describe '#expression' do
     include AST::Sexp
 
@@ -51,5 +59,68 @@ RSpec.describe RuboCop::AST::CasgnNode do
     let(:source) { 'VAR = value' }
 
     it { is_expected.to eq(s(:send, nil, :value)) }
+  end
+
+  describe '#module_name?' do
+    context 'with a constant with only uppercase letters' do
+      let(:source) { 'VAR = value' }
+
+      it { expect(casgn_node).not_to be_module_name }
+    end
+
+    context 'with a constant with a lowercase letter' do
+      let(:source) { '::Foo::Bar = value' }
+
+      it { expect(casgn_node).to be_module_name }
+    end
+  end
+
+  describe '#absolute?' do
+    context 'with a constant starting with ::' do
+      let(:source) { '::VAR' }
+
+      it { expect(casgn_node).to be_absolute }
+    end
+
+    context 'with a constant not starting with ::' do
+      let(:source) { 'Foo::Bar::BAZ' }
+
+      it { expect(casgn_node).not_to be_absolute }
+    end
+
+    context 'with a non-namespaced constant' do
+      let(:source) { 'Foo' }
+
+      it { expect(casgn_node).not_to be_absolute }
+    end
+  end
+
+  describe '#relative?' do
+    context 'with a constant starting with ::' do
+      let(:source) { '::VAR' }
+
+      it { expect(casgn_node).not_to be_relative }
+    end
+
+    context 'with a constant not starting with ::' do
+      let(:source) { 'Foo::Bar::BAZ' }
+
+      it { expect(casgn_node).to be_relative }
+    end
+
+    context 'with a non-namespaced constant' do
+      let(:source) { 'Foo' }
+
+      it { expect(casgn_node).to be_relative }
+    end
+  end
+
+  describe '#each_path' do
+    let(:source) { '::Foo::Bar::BAZ = value' }
+
+    it 'yields all parts of the namespace' do
+      expect(casgn_node.each_path.map(&:type)).to eq %i[cbase const const]
+      expect(casgn_node.each_path.to_a.last(2).map(&:short_name)).to eq %i[Foo Bar]
+    end
   end
 end
