c34aed5ddac9c616a9e314f8bf1fd628121ba7ea
Daniel Vandersluis <daniel.vandersluis@gmail.com>
2024-11-11 14:49:50 -0500

    [#Fix 334] Make `body` consistent for `RescueNode`, `EnsureNode` and `KeywordBeginNode`.

--- spec/rubocop/ast/ensure_node_spec.rb
+++ spec/rubocop/ast/ensure_node_spec.rb
@@ -11,6 +11,54 @@ RSpec.describe RuboCop::AST::EnsureNode do
     it { expect(ensure_node).to be_a(described_class) }
   end
 
+  describe '#body' do
+    subject(:body) { ensure_node.body }
+
+    context 'when there is no body' do
+      let(:source) { 'begin; ensure; ensurebody; end' }
+
+      it { is_expected.to be_nil }
+    end
+
+    context 'when the body is a single line' do
+      let(:source) { 'begin; >>beginbody<<; ensure; ensurebody; end' }
+
+      it { is_expected.to eq(node) }
+    end
+
+    context 'when the body is multiple lines' do
+      let(:source) { 'begin; >>foo<<; bar; ensure; ensurebody; end' }
+
+      it 'returns a begin node' do
+        expect(body).to be_begin_type
+        expect(body.children).to include(node)
+      end
+    end
+
+    context 'with `rescue`' do
+      context 'when there is no body' do
+        let(:source) { 'begin; rescue; rescuebody; ensure; ensurebody; end' }
+
+        it { is_expected.to be_nil }
+      end
+
+      context 'when the body is a single line' do
+        let(:source) { 'begin; >>beginbody<<; rescue; rescuebody; ensure; ensurebody; end' }
+
+        it { is_expected.to eq(node) }
+      end
+
+      context 'when the body is multiple lines' do
+        let(:source) { 'begin; >>foo<<; bar; rescue; rescuebody; ensure; ensurebody; end' }
+
+        it 'returns a begin node' do
+          expect(body).to be_begin_type
+          expect(body.children).to include(node)
+        end
+      end
+    end
+  end
+
   describe '#branch' do
     let(:source) { 'begin; beginbody; ensure; >>ensurebody<<; end' }
 
